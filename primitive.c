294f26904f466bdc0394653934e907cc6fcd13bd
PickledChair <ubatamamoon@gmail.com>
2022-12-04 01:55:35 +0900

    組み込み手続きオブジェクトをマクロで定義できるようにした

--- primitive.c
+++ primitive.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "fzscheme.h"
 
+#define DEFINE_PRIM_OBJ(FN_NAME)      \
+  Object *FN_NAME##_obj =             \
+      &(Object){.tag = OBJ_PRIMITIVE, \
+                .fields_of = {.primitive = {.symbol = NULL, .fn = FN_NAME}}};
+
 static Object *prim_car(Object *obj) {
   Object *fst = CAR(obj);
   if (fst->tag != OBJ_CELL) {
@@ -10,32 +15,17 @@ static Object *prim_car(Object *obj) {
   }
   return CAR(fst);
 }
-Object *prim_car_obj = &(Object){
-  .tag = OBJ_PRIMITIVE,
-  .fields_of = {
-    .primitive = { .symbol = NULL, .fn = prim_car }
-  }
-};
+DEFINE_PRIM_OBJ(prim_car)
 
 static Object *prim_display(Object *obj) {
   Object *fst = CAR(obj);
   print_obj(fst);
   return UNDEF;
 }
-Object *prim_display_obj = &(Object){
-  .tag = OBJ_PRIMITIVE,
-  .fields_of = {
-    .primitive = { .symbol = NULL, .fn = prim_display }
-  }
-};
+DEFINE_PRIM_OBJ(prim_display)
 
 static Object *prim_newline(Object *obj) {
   putchar('\n');
   return UNDEF;
 }
-Object *prim_newline_obj = &(Object){
-  .tag = OBJ_PRIMITIVE,
-  .fields_of = {
-    .primitive = { .symbol = NULL, .fn = prim_newline }
-  }
-};
+DEFINE_PRIM_OBJ(prim_newline)
